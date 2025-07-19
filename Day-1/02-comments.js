ec71f75117fc01e1d4324f3f4afdb9ad47aa0502
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2025-07-19 11:48:50 -0700

    Delete Day-1 directory

--- Day-1/02-comments.js
+++
@@ -1,7 +0,0 @@
-// This is a single line comment.
-
-/* 
-This is
-a multiline
-comment.
- */
--- Day-1/04-Interaction/script.js
+++
@@ -1,6 +0,0 @@
-alert("Ready for interaction!");
-
-let name = prompt("Enter your name: ", "name");
-let isConfirmed = confirm("Is name correct? ");
-alert(`${name} is confirmed.`);
-alert(`Hello ${name}!`);
