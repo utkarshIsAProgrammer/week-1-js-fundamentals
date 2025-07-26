368876bb9709ab992abd6abbbe0f1f0e024ef7c5
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2025-07-26 01:34:50 -0700

    Added problem1 second solution

--- Day-5/Codewars-Challenge/problem1-2.js
+++ Day-5/Codewars-Challenge/problem1-2.js
@@ -6,4 +6,22 @@
 // If a = [1, 2] and b = [1], the result should be [2].
 // If a = [1, 2, 2, 2, 3] and b = [2], the result should be [1, 3].
 
-function arrayDiff(a, b) {}
+function arrayDiff(a, b) {
+	let result = [];
+	for (let i = 0; i < a.length; i++) {
+		let found = false;
+		for (let j = 0; j < b.length; j++) {
+			if (a[i] === b[j]) {
+				found = true;
+				break;
+			}
+		}
+		if (!found) {
+			result.push(a[i]);
+		}
+	}
+	return result;
+}
+
+let result = arrayDiff((a = [1, 2, 2, 2, 3]), (b = [3]));
+console.log(result);
